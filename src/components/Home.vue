bffa22cf441c2fb74f7409ef45917ffdbddf8811
hninhnin0929 <hninhnin0929@gmail.com>
2023-06-18 00:40:04 +0900

    check box, radio button with label, data property, two-way binding

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,13 +1,26 @@
 <template>
     <div>
-        <h2>Get input field value</h2>
-        <input type="text" placeholder="enter email" 
-            v-model="email"/>
-        <br/>
-        <input type="password" placeholder="enter password" 
-            v-model="password"/>
+        <h1>Get Checkbox & Radio Button Value</h1>
+        <h3>Technology</h3>
+
+        <label for="java">Java</label>
+        <input type="checkbox" value="java" v-model="technology" id="java" />
+
+        <label for="node">Node</label>
+        <input type="checkbox" value="node" v-model="technology" id="java" />
+
+        <label for="php">Java</label>
+        <input type="checkbox" value="php" v-model="technology" id="php" />
+
         <br/><br/>
-        <button type="button" v-on:click="getData()">Get Data</button>
+        <h3>Profession</h3>
+        <label for="student">Student</label>
+        <input type="radio" value="student" id="student" v-model="profession" name="profession"/>
+        <label for="developer">Developer</label>
+        <input type="radio" value="developer" id="developer" v-model="profession" name="profession"/>
+
+        <h4>Technology : {{ technology }}</h4>
+        <h4>I am a : {{ profession }}</h4>
     </div>
 </template>
 
@@ -16,13 +29,13 @@
         name: "Home",       
         data(){
             return{
-               email: null,
-               password: null
+               technology : [],
+               profession : null
             }
         },
         methods: {
             getData(){
-               console.warn("values : ", this.email, this.password);
+               
             },
         }
     }
