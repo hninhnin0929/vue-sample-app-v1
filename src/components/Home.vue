18eb79a9c9720648a622369750d11dfa4d57719c
hninhnin0929 <hninhnin0929@gmail.com>
2023-06-17 23:26:11 +0900

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h1>Home Component {{ "Hello".length }}</h1>
+        <h1>Email : {{ getData().email }}</h1>
+        <h1>Name : {{ getData().name }}</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Home",
+        
+        data(){
+            return{
+                email: "anil@test.com", 
+            }
+        },
+        methods: {
+            getData(){
+                return {
+                    name: "hnin",
+                    email: this.email
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+h1{
+    color: yellowgreen
+}
+</style>
