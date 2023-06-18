2f5bae8e710aaeefa204afa41869978a3f5bfe82
hninhnin0929 <hninhnin0929@gmail.com>
2023-06-18 23:25:18 +0900

    form validation, add err props, apply loop over form files and collect errs

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,44 +1,7 @@
 <template>
     <div>
         
-        <h1>Simple Form</h1>
-        <p>{{ form }}</p>
-        <form>
-            <label>Email : </label>
-            <input type="text" placeholder="enter email" v-model="form.email"/>
-            <br /><br />
-
-            <label>Password : </label>
-            <input type="password" placeholder="enter password" v-model="form.password"/>
-            <br /><br />
-
-            <select v-model="form.country">
-                <option>India</option>
-                <option>USA</option>
-            </select>
-            <br /><br />
-
-            <h3>Gender</h3>
-            <label>Male : </label>
-            <input type="radio" name="gender" value="male" v-model="form.gender"/>
-            <br /><br />
-
-            <label>Female : </label>
-            <input type="radio" name="gender" value="female" v-model="form.gender"/>
-            <br /><br />
-
-            <h3>Technology</h3>
-            <label>Java : </label>
-            <input type="checkbox" value="java" v-model="form.technology"/>
-            <br /><br />
-
-            <label>.Net : </label>
-            <input type="checkbox"  value=".net" v-model="form.technology"/>
-            <br /><br />
-
-            <button type="button" v-on:click="login">Login</button>
-
-        </form>
+        <h1>Home Component</h1>
         
     </div>
 </template>
@@ -54,18 +17,12 @@
         },      
         data(){
             return{
-                form: {
-                    email: "",
-                    password: "",
-                    country: "",
-                    gender: "",
-                    technology: "",
-                }
+                
             }
         },
         methods: {
-            login(){
-                console.warn(this.form);
+            getData(){
+                
             }
         }
     }
---
+++ src/components/SimpleForm.vue
@@ -0,0 +1,94 @@
+<template>
+    <div>
+        
+        <h1>Simple Form</h1>
+        <ul>
+            <li v-for="item in error" v-bind:key="item">
+                {{ item }} is not valid
+            </li>
+        </ul>
+        <form>
+            <label>Email : </label>
+            <input type="text" placeholder="enter email" v-model="form.email"/>
+            <br /><br />
+
+            <label>Password : </label>
+            <input type="password" placeholder="enter password" v-model="form.password"/>
+            <br /><br />
+
+            <select v-model="form.country">
+                <option>India</option>
+                <option>USA</option>
+            </select>
+            <br /><br />
+
+            <h3>Gender</h3>
+            <label>Male : </label>
+            <input type="radio" name="gender" value="male" v-model="form.gender"/>
+            <br /><br />
+
+            <label>Female : </label>
+            <input type="radio" name="gender" value="female" v-model="form.gender"/>
+            <br /><br />
+
+            <h3>Technology</h3>
+            <label>Java : </label>
+            <input type="checkbox" value="java" v-model="form.technology"/>
+            <br /><br />
+
+            <label>.Net : </label>
+            <input type="checkbox"  value=".net" v-model="form.technology"/>
+            <br /><br />
+
+            <button type="button" v-on:click="login">Login</button>
+
+        </form>
+        
+    </div>
+</template>
+
+<script>
+
+
+    export default {
+
+        name: "Simple Form", 
+        components: {
+            
+        },      
+        data(){
+            return{
+                form: {
+                    email: "",
+                    password: "",
+                    country: "",
+                    gender: "",
+                    technology: "",
+                },
+                error: [],
+            }
+        },
+        methods: {
+            login(){
+                
+                this.error = [];
+                for(const item in this.form)
+                {
+                    if(this.form[item] === "" || this.form[item].length === 0){
+                        this.error.push(item);
+                    }                                    
+                }
+                if(this.error.length === 0){
+                        alert("Data has been submitted")
+                }    
+                console.log("error"+this.error);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+h1{
+    color: yellowgreen
+}
+</style>
