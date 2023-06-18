9f1747df88dcf705aca525ed659b97c8ae832680
hninhnin0929 <hninhnin0929@gmail.com>
2023-06-18 16:13:13 +0900

    class binding

---
+++ src/components/Class-binding.vue
@@ -0,0 +1,60 @@
+<template>
+    <div>
+        
+        <h1>CSS binding</h1>
+        <h2 class="staticClass" :class="adjustStyle">Home Component</h2>
+        <button v-on:click=" colorfull =!colorfull">Apply Style</button>
+    </div>
+</template>
+
+<script>
+
+
+    export default {
+
+        name: "Class-binding", 
+        components: {
+            
+        },      
+        data(){
+            return{
+               colorfull : true 
+            }
+        },
+        computed: {
+            adjustStyle(){
+                return {
+                    green: this.colorfull,
+                    err: true,
+                    other: false
+                }
+            }
+        },
+        methods: {
+            getData(name){
+                
+            }
+        }
+    }
+</script>
+
+<style scoped>
+h1{
+    color: yellowgreen
+}
+.green{
+    background-color: green;
+    width : 200px;
+    padding: 10px;
+    color: #fff;
+}
+.staticClass{
+    color: blue;
+}
+.err{
+    color: red;
+}
+.other{
+    font-size: 70px;
+}
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,8 @@
 <template>
     <div>
         
-        <h1>{{ text }}</h1>
-        {{ tag }}
-        <div v-html="tag"></div>
-        <div v-html="tag + tag2"></div>
-        <div v-html="tag + tag2 + tag3"></div>
+        <h1>Reuse Component</h1>
+        
     </div>
 </template>
 
@@ -20,10 +17,7 @@
         },      
         data(){
             return{
-                text: "hnin hnin",
-                tag: "<h2>HNIN</h2>",
-                tag2: "<h2>Snow<h2>",
-                tag3: "<h2>Lay<h2>"
+                
             }
         },
         methods: {
