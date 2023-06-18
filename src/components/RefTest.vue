d438f1bc2d73687978b660aab7b87f9e79ba4648
hninhnin0929 <hninhnin0929@gmail.com>
2023-06-18 18:25:25 +0900

    ref, make input field and apply ref, operations with ref

---
+++ src/components/RefTest.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        
+        <h1>Ref in Vue js</h1>
+        <input type="text" ref="input"/>
+        <button v-on:click="getData">Click Me</button>
+    </div>
+</template>
+
+<script>
+
+
+    export default {
+
+        name: "RefTest", 
+        components: {
+            
+        },      
+        data(){
+            return{
+                
+            }
+        },
+        methods: {
+            getData(){
+                this.$refs.input.focus();
+                this.$refs.input.value = "Hi";
+                this.$refs.input.style.color = "red";
+                console.warn(this.$refs.input.value);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+h1{
+    color: yellowgreen
+}
+</style>
