ac7d64cd84f9770827074d5503cd9b071b1f643b
hninhnin0929 <hninhnin0929@gmail.com>
2023-06-20 16:46:31 +0900

    computed property

---
+++ src/components/ComputedProperty.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        
+        <h1>Computed Property {{ dollors }}</h1>
+        <!-- <h2>{{ (dollors * rupeeVal) - discount }}</h2> -->
+        <h2>{{ getResult }}</h2>
+        <h2>{{ getData() }}</h2>
+    </div>
+</template>
+
+<script>
+
+    export default {
+
+        name: "ComputedProperty", 
+        components: {
+
+        },      
+        data(){
+            return{
+                dollors: 100,
+                rupeeVal: 70,
+                discount: 10
+            }
+        },
+        methods: {
+            getData(){
+                return (this.dollors * this.rupeeVal) - this.discount;
+            }
+        },
+        computed: {
+            getResult(){
+                return (this.dollors * this.rupeeVal) - this.discount;
+            }
+        },
+    }
+</script>
+
+<style scoped>
+h1{
+    color: orange
+}
+</style>
+
+<!-- computed properties are cached based on their reactive dependencies. -->
