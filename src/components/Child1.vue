6003a633abae6bc4b180de273aad78c53124f190
hninhnin0929 <hninhnin0929@gmail.com>
2023-06-21 15:50:22 +0900

    problem with normal slot, example of named slot

--- src/components/Child1.vue
+++ src/components/Child1.vue
@@ -1,37 +1,56 @@
 <template>
-    <div>
-        
+    <div class="cart">
+
         <!-- <h1>{{ name }}</h1> -->
-        <slot><h1>Default Data</h1></slot>
+        <!-- <slot><h1>Default Data</h1></slot>
+        <slot><h1>Default Data</h1></slot> -->
+            
+        <header>
+            <slot name="header">Default Data</slot>
+        </header>
+        <content>
+            <slot name="main">Default Data</slot>
+        </content>
+        <footer>
+            <slot name="footer">Default Data</slot>
+        </footer>
+
     </div>
 </template>
 
 <script>
 
-    export default {
+export default {
 
-        name: "Child1", 
-        props: {
-            name: String,
-        },
-        components: {
+    name: "Child1",
+    props: {
+        name: String,
+    },
+    components: {
 
-        },      
-        data(){
-            return{
-                
-            }
-        },
-        methods: {
-            getData(){
-                
-            }
+    },
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+        getData() {
+
         }
     }
+}
 </script>
 
 <style scoped>
-h1{
+h1 {
     color: orange
+}
+.cart{
+    border: 1px solid;
+    width: 130px;
+    padding: 20px;
+    margin: 20px;
+    text-align: center;
 }
 </style>
--- src/components/Slots.vue
+++ src/components/Slots.vue
@@ -1,36 +1,71 @@
 <template>
     <div>
-        
-        <h1>Slot</h1>
-        <Child1><h1>Code Step by Step</h1></Child1>
-        <Child1><h2>hnin</h2></Child1>
-        <Child1></Child1>
+
+        <!-- <h1>Slot</h1> -->
+        <!-- <Child1><h1>Code Step by Step</h1></Child1>
+        <Child1><h2>hnin</h2></Child1> 
+        <Child1></Child1>-->
+
+
+        <Child1>
+            <template v-slot:header>
+                <h1>Peter</h1>
+            </template>
+            <template v-slot:main>
+                <p>Code Step By Step</p>
+            </template>
+            <template v-slot:footer>
+                <button>Buy Now</button>
+            </template>            
+        </Child1>
+        <Child1>
+            <template v-slot:header>
+                <h1>Sam</h1>
+            </template>
+            <template v-slot:main>
+                <p>Code Step By Step</p>
+            </template>
+            <template v-slot:footer>
+                <button>Buy Now</button>
+            </template>            
+        </Child1>
+        <Child1>
+            <template v-slot:header>
+                <h1>Snow</h1>
+            </template>
+            <template v-slot:main>
+                <p>Code Step By Step</p>
+            </template>
+            <template v-slot:footer>
+                <button>Buy Now</button>
+            </template>            
+        </Child1>
     </div>
 </template>
 
 <script>
-    import Child1 from './Child1.vue'
-    export default {
+import Child1 from './Child1.vue'
+export default {
 
-        name: "Slots", 
-        components: {
-            Child1
-        },      
-        data(){
-            return{
-                
-            }
-        },
-        methods: {
-            getData(){
-                
-            }
+    name: "Slots",
+    components: {
+        Child1
+    },
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+        getData() {
+
         }
     }
+}
 </script>
 
 <style scoped>
-h1{
+h1 {
     color: orange
 }
 </style>
