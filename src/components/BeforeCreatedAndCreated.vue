2573916669182d4a62d5c5b24fa9cd9ab40eae1f
hninhnin0929 <hninhnin0929@gmail.com>
2023-06-23 17:05:56 +0900

    beforecreated and created

---
+++ src/components/BeforeCreatedAndCreated.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        
+        <h1>beforeCreated and created {{ name }}</h1>
+    </div>
+</template>
+
+<script>
+
+    export default {
+
+        name: "BeforeCreatedAndCreated", 
+        components: {
+
+        },      
+        data(){
+            console.warn("Data is created")
+            return{
+                name: 'Hnin',
+            }
+        },
+        methods: {
+            getData(){
+                
+            }
+        },
+        beforeCreate(){
+            
+            alert("beforeCreated");
+        },
+        created(){
+            this.name = "Snow";
+            alert("created")
+        },
+    }
+</script>
+
+<style scoped>
+h1{
+    color: orange
+}
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         
-        <h1>Home</h1>
+        <h1>Home Components</h1>
     </div>
 </template>
 
