ea4193dcb78b379ece86750b1db19e6026a6ed90
hninhnin0929 <hninhnin0929@gmail.com>
2023-06-23 15:58:56 +0900

    teleport components

---
+++ src/components/Footer.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="footer">
+
+        <h3>Footer of App</h3>
+        <p>Copy rights @ Code Step By Step</p>
+    </div>
+</template>
+
+<script>
+
+export default {
+
+    name: "Footer",
+    components: {
+        
+    },
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+        getData() {
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+h1 {
+    color: orange
+}
+.footer {
+    background-color: antiquewhite;
+    color: darkmagenta;
+    position: absolute;
+    width: 100%;
+    left: 0;
+    bottom: 0;
+    text-align: center;
+}
+</style>
---
+++ src/components/TeleportComponent.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        
+        <h1>Teleport Components</h1>
+        
+        <teleport to="#footer">
+            <Footer />
+        </teleport>
+    </div>
+</template>
+
+<script>
+
+    import Footer from './Footer.vue';
+
+    export default {
+
+        name: "TeleportComponent", 
+        components: {
+            Footer
+        },      
+        data(){
+            return{
+                
+            }
+        },
+        methods: {
+            getData(){
+                
+            }
+        }
+    }
+</script>
+
+<style scoped>
+h1{
+    color: orange
+}
+</style>
