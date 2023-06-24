224119b3ed0942e93fbd6bbc605776d7eb19645f
hninhnin0929 <hninhnin0929@gmail.com>
2023-06-25 00:27:33 +0900

    make list in for loop, show image, apply style

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,9 +2,15 @@
     <div>
         
         <h1>Call API</h1>
-        <p v-for="item in list" :key="item.id">
+        <!-- <p v-for="item in list" :key="item.id">
            {{ item.id }}---{{ item.email }}---{{ item.first_name }} 
-        </p>
+        </p> -->
+        <ul class="item" v-for="item in list" :key="item.id">
+            <li>{{ item.id }}</li>
+            <li>{{ item.email }}</li>
+            <li>{{ item.first_name }}</li>
+            <li><img :src="item.avatar"/></li>
+        </ul>
     </div>
 </template>
 
@@ -39,5 +45,18 @@
 <style scoped>
 h1{
     color: orange
+}
+.item{
+    display: flex;
+}
+.item img{
+    width: 50px;
+}
+.item li{
+    display: inline-block;
+    border: 1px solid;
+    width: 180px;
+    text-align: center;
+    padding: 5px;
 }
 </style>
